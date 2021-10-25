46674d84d71554c3ba82b42ea1c8f0b288209cc2
HemanthCU <hech9374@colorado.edu>
2021-10-25 13:53:42 -0600

    Updated with comments

--- httpechosrv.c
+++ httpechosrv.c
@@ -12,7 +12,6 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <pthread.h>
-#include <sys/socket.h>
 
 #define MAXLINE  8192  /* max text line length */
 #define MAXBUF   8192  /* max I/O buffer size */
@@ -39,11 +38,16 @@ int main(int argc, char **argv) {
     while (1) {
         connfdp = malloc(sizeof(int));
         *connfdp = accept(listenfd, (struct sockaddr*)&clientaddr, &clientlen);
+        // Create a new thread only if a new connection arrives
         if (*connfdp >= 0)
             pthread_create(&tid, NULL, thread, connfdp);
     }
 }
 
+/* 
+ * getContentType - gets the content type that needs to be added to HTTP response
+ * Returns NULL in case of failure 
+ */
 char* getContentType(char *tgtpath) {
     char *temp1 = (char*) malloc (100*sizeof(char));
     char *temp2 = (char*) malloc (100*sizeof(char));
@@ -86,29 +90,31 @@ void * thread(void * vargp) {
     free(vargp);
     // Process the header to get details of request
     size_t n;
-    int keepalive = 0;
-    int first = 1;
-    int msgsz;
-    char buf[MAXLINE];
-    char *resp = (char*) malloc (MAXREAD*sizeof(char));
-    unsigned char msg[MAXREAD + 1];
-    char *context = NULL;
-    char *comd;
-    char *host;
-    char *temp = NULL;
-    char *tgtpath;
-    char *tgtpath1 = (char*) malloc (100*sizeof(char));
-    char *httpver;
-    char *contType;
-    char *postdata;
+    int keepalive = 0; /* Denotes if the current request is persistent */
+    int first = 1; /* Denotes if this is the first execution of the while loop */
+    int msgsz; /* Size of data read from the file */
+    char buf[MAXLINE]; /* Request full message */
+    char *resp = (char*) malloc (MAXREAD*sizeof(char)); /* Response header */
+    unsigned char msg[MAXREAD + 1]; /* Data read from the file */
+    char *context = NULL; /* Pointer used for string tokenizer */
+    char *comd; /* Incoming HTTP command */
+    char *host; /* Incoming HTTP host */
+    char *temp = NULL; /* Temporary pointer to check if connection is persistent */
+    char *tgtpath; /* Incoming HTTP URL */
+    char *tgtpath1 = (char*) malloc (100*sizeof(char)); /* Path to the file referenced by URL */
+    char *httpver; /* Incoming HTTP version */
+    char *contType; /* Content type of the data being returned */
+    char *postdata; /* Postdata to be appended to the request */
     char c;
-    FILE *fp;
-    while(keepalive || first--) {
+    FILE *fp; /* File descriptor to open the file */
+    while (keepalive || first--) {
         printf("Waiting for data\n");
         n = read(connfd, buf, MAXLINE);
         if ((int)n >= 0) {
             comd = strtok_r(buf, " \t\r\n\v\f", &context);
             tgtpath = strtok_r(NULL, " \t\r\n\v\f", &context);
+            // Default Page is handled here by looking for an index.html file at the given directory.
+            // If it isn't found, the code will lead to 404 Not found error later.
             if (tgtpath[strlen(tgtpath) - 1] == '/')
                 sprintf(tgtpath1, "./www%sindex.html", tgtpath);
             else
@@ -117,6 +123,7 @@ void * thread(void * vargp) {
             host = strtok_r(NULL, " \t\r\n\v\f", &context);
             host = strtok_r(NULL, " \t\r\n\v\f", &context);
 
+            // Based on the incoming header data, decide if the connection must be persistent or not.
             if (strcmp(httpver, "HTTP/1.1") == 0) {
                 c = context[1];
                 if (c != '\r') {
@@ -134,15 +141,18 @@ void * thread(void * vargp) {
                 keepalive = 0;
             }
             contType = getContentType(tgtpath1);
-            printf("comd=%s tgtpath=%s httpver=%s host=%s keepalive=%d \n", comd, tgtpath1, httpver, host, keepalive);
+            //printf("comd=%s tgtpath=%s httpver=%s host=%s keepalive=%d \n", comd, tgtpath1, httpver, host, keepalive);
             // Choose what to perform based on comd
             if (contType == NULL || (strcmp(comd, "POST") == 0 && strcmp(contType, "text/html") != 0)) {
-                // TODO: Need to return webpage with error
+                // Process bad requests from the client
+                // 1) Request to a file type that isn't supported
+                // 2) Request to non-html files in POST requests
                 printf("ERROR in requested data type\n");
                 sprintf(msg, "<html><head><title>400 Bad Request</title></head><body><h2>400 Bad Request</h2></body></html>");
                 sprintf(resp, "%s 400 Bad Request\r\nContent-Type:text/html\r\nContent-Length:%d\r\n\r\n%s", httpver, (int)strlen(msg), msg);
                 write(connfd, resp, strlen(resp));
             } else if (strcmp(comd, "GET") == 0) {
+                // Handle GET command - Read data from the tgtpath1 and respond with header data and the document
                 if (contType[0] != 't')
                     fp = fopen(tgtpath1, "rb");
                 else
@@ -155,11 +165,14 @@ void * thread(void * vargp) {
                     write(connfd, msg, msgsz);
                     fclose(fp);
                 } else {
+                    // Process requests to files that don't exist
                     sprintf(msg, "<html><head><title>404 File Not Found</title></head><body><h2>404 File Not Found</h2></body></html>");
                     sprintf(resp, "%s 404 File Not Found\r\nContent-Type:text/html\r\nContent-Length:%d\r\n\r\n%s", httpver, (int)strlen(msg), msg);
                     write(connfd, resp, strlen(resp));
                 }
             } else if (strcmp(comd, "POST") == 0) {
+                // Handle POST command - Read data from the tgtpath1 and respond with header data
+                // and the document appended with the postdata sent in the request
                 postdata = strtok_r(NULL, " \t\r\n\v\f", &context);
                 if (contType[0] != 't')
                     fp = fopen(tgtpath1, "rb");
@@ -174,11 +187,13 @@ void * thread(void * vargp) {
                     write(connfd, msg, msgsz);
                     fclose(fp);
                 } else {
+                    // Process requests to files that don't exist
                     sprintf(msg, "<html><head><title>404 File Not Found</title></head><body><h2>404 File Not Found</h2></body></html>");
                     sprintf(resp, "%s 404 File Not Found\r\nContent-Type:text/html\r\nContent-Length:%d\r\n\r\n%s", httpver, (int)strlen(msg), msg);
                     write(connfd, resp, strlen(resp));
                 }
             } else if (strcmp(comd, "HEAD") == 0) {
+                // Handle HEAD command - Read data from the tgtpath1 and respond with header data only
                 if (contType[0] != 't')
                     fp = fopen(tgtpath1, "rb");
                 else
@@ -186,15 +201,17 @@ void * thread(void * vargp) {
                 if (fp != NULL) {
                     fseek(fp, 0, SEEK_SET);
                     msgsz = fread(msg, 1, MAXREAD, fp);
-                    sprintf(resp, "%s 200 Document Follows\r\nContent-Type:%s\r\nContent-Length:%d\r\n\r\n%s", httpver, contType, msgsz, msg);
+                    sprintf(resp, "%s 200 Document Follows\r\nContent-Type:%s\r\nContent-Length:%d\r\n\r\n", httpver, contType, msgsz);
                     write(connfd, resp, strlen(resp));
                     fclose(fp);
                 } else {
+                    // Process requests to files that don't exist
                     sprintf(msg, "<html><head><title>404 File Not Found</title></head><body><h2>404 File Not Found</h2></body></html>");
                     sprintf(resp, "%s 404 File Not Found\r\nContent-Type:text/html\r\nContent-Length:%d\r\n\r\n%s", httpver, (int)strlen(msg), msg);
                     write(connfd, resp, strlen(resp));
                 }
             } else {
+                // Process Internal Server errors like HTTP commands that are not supported
                 sprintf(msg, "<html><head><title>500 Internal Server Error</title></head><body><h2>500 Internal Server Error</h2></body></html>");
                 sprintf(resp, "%s 500 Internal Server Error\r\nContent-Type:text/html\r\nContent-Length:%d\r\n\r\n%s", httpver, (int)strlen(msg), msg);
                 write(connfd, resp, strlen(resp));
@@ -224,6 +241,8 @@ int open_listenfd(int port) {
     if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, 
                     (const void *)&optval , sizeof(int)) < 0)
         return -1;
+
+    /* Sets a timeout of 10 secs. */
     struct timeval tv;
     tv.tv_sec = 10;
     tv.tv_usec = 0;
